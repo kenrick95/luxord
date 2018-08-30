eccffef5329f1a486c93679ffdf56d2f1202b4ab
Kenrick <kenrick95@gmail.com>
2018-08-30 21:46:47 +0800

    Add more test

--- src/timer/switchTimer.test.ts
+++ src/timer/switchTimer.test.ts
@@ -1,64 +1,118 @@
 import { advanceBy, advanceTo, clear } from 'jest-date-mock';
 import SwitchTimer, { TIMER_ID } from './switchTimer';
 
-jest.useFakeTimers();
+const alertSpy = jest
+  .spyOn(window, 'alert')
+  .mockImplementation((...args) => console.info.apply(null, args));
 
 let switchTimer: SwitchTimer;
 const timerAmount = 1000;
 const now = Date.now();
 beforeEach(() => {
+  jest.useFakeTimers();
   advanceTo(now);
 });
 afterEach(() => {
+  jest.clearAllTimers();
+  alertSpy.mockClear();
   clear();
 });
-describe('switchTimer initial state', () => {
+test('switchTimer initial state', () => {
   const switchTimer = new SwitchTimer(timerAmount);
-  test('Initially, none should be active', () => {
-    expect(switchTimer.activeTimerId).toBe(TIMER_ID.NONE);
-  });
+  expect(switchTimer.activeTimerId).toBe(TIMER_ID.NONE);
 });
 describe('switchTimer Player 1 starts', () => {
-  beforeEach(() => {
+  test('initial state', () => {
     switchTimer = new SwitchTimer(timerAmount);
     switchTimer.activeTimerId = TIMER_ID.ONE;
     switchTimer.start();
-  });
-  test('Timer Player 1 should be active', () => {
     expect(switchTimer.timers[TIMER_ID.ONE].isActive).toBe(true);
-  });
-  test('Timer Player 1 msLeft should be 1000ms', () => {
     expect(switchTimer.timers[TIMER_ID.ONE].msLeft).toBe(1000);
-  });
-  test('Timer Player 2 should be not active', () => {
     expect(switchTimer.timers[TIMER_ID.TWO].isActive).toBe(false);
-  });
-  test('Timer Player 2 msLeft should be 1000ms', () => {
     expect(switchTimer.timers[TIMER_ID.ONE].msLeft).toBe(1000);
+    expect(alertSpy).not.toBeCalled();
   });
-});
-describe('switchTimer Player 1 starts, then after 500 ms', () => {
-  beforeEach(() => {
+  test('after 400 ms', () => {
     switchTimer = new SwitchTimer(timerAmount);
     switchTimer.activeTimerId = TIMER_ID.ONE;
     switchTimer.start();
-    advanceTo(now + 500);
-  });
-  afterEach(() => {
-    advanceTo(now);
-  });
-  jest.advanceTimersByTime(500);
+    advanceTo(now + 400);
+    jest.advanceTimersByTime(400);
 
-  test('Timer Player 1 should be active', () => {
     expect(switchTimer.timers[TIMER_ID.ONE].isActive).toBe(true);
-  });
-  test('Timer Player 1 msLeft should be 500ms', () => {
-    expect(switchTimer.timers[TIMER_ID.ONE].msLeft).toBe(500);
-  });
-  test('Timer Player 2 should be not active', () => {
+    expect(switchTimer.timers[TIMER_ID.ONE].msLeft).toBe(600);
     expect(switchTimer.timers[TIMER_ID.TWO].isActive).toBe(false);
-  });
-  test('Timer Player 2 msLeft should be 1000ms', () => {
     expect(switchTimer.timers[TIMER_ID.TWO].msLeft).toBe(1000);
+    expect(alertSpy).not.toBeCalled();
+  });
+
+  test('after 400 ms, switch', () => {
+    switchTimer = new SwitchTimer(timerAmount);
+    switchTimer.activeTimerId = TIMER_ID.ONE;
+    switchTimer.start();
+    advanceTo(now + 400);
+    jest.advanceTimersByTime(400);
+    switchTimer.switch();
+
+    expect(switchTimer.timers[TIMER_ID.ONE].isActive).toBe(false);
+    expect(switchTimer.timers[TIMER_ID.ONE].msLeft).toBe(600);
+    expect(switchTimer.timers[TIMER_ID.TWO].isActive).toBe(true);
+    expect(switchTimer.timers[TIMER_ID.TWO].msLeft).toBe(1000);
+    expect(alertSpy).not.toBeCalled();
+  });
+
+  test('after 400 ms, switch, and wait for 200 ms', () => {
+    switchTimer = new SwitchTimer(timerAmount);
+    switchTimer.activeTimerId = TIMER_ID.ONE;
+    switchTimer.start();
+    advanceTo(now + 400);
+    jest.advanceTimersByTime(400);
+    switchTimer.switch();
+    advanceTo(now + 600);
+    jest.advanceTimersByTime(200);
+
+    expect(switchTimer.timers[TIMER_ID.ONE].isActive).toBe(false);
+    expect(switchTimer.timers[TIMER_ID.ONE].msLeft).toBe(600);
+    expect(switchTimer.timers[TIMER_ID.TWO].isActive).toBe(true);
+    expect(switchTimer.timers[TIMER_ID.TWO].msLeft).toBe(800);
+    expect(alertSpy).not.toBeCalled();
+  });
+
+  test('after 400 ms, switch, wait for 200 ms, and switch', () => {
+    switchTimer = new SwitchTimer(timerAmount);
+    switchTimer.activeTimerId = TIMER_ID.ONE;
+    switchTimer.start();
+    advanceTo(now + 400);
+    jest.advanceTimersByTime(400);
+    switchTimer.switch();
+    advanceTo(now + 600);
+    jest.advanceTimersByTime(200);
+    switchTimer.switch();
+
+    expect(switchTimer.timers[TIMER_ID.ONE].isActive).toBe(true);
+    expect(switchTimer.timers[TIMER_ID.ONE].msLeft).toBe(600);
+    expect(switchTimer.timers[TIMER_ID.TWO].isActive).toBe(false);
+    expect(switchTimer.timers[TIMER_ID.TWO].msLeft).toBe(800);
+    expect(alertSpy).not.toBeCalled();
+  });
+
+  test('after 400 ms, switch, wait for 200 ms, switch, and wait 600 ms', () => {
+    switchTimer = new SwitchTimer(timerAmount);
+    switchTimer.activeTimerId = TIMER_ID.ONE;
+    switchTimer.start();
+    advanceTo(now + 400);
+    jest.advanceTimersByTime(400);
+    switchTimer.switch();
+    advanceTo(now + 600);
+    jest.advanceTimersByTime(200);
+    switchTimer.switch();
+    advanceTo(now + 1200);
+    jest.advanceTimersByTime(600);
+
+    expect(switchTimer.timers[TIMER_ID.ONE].isActive).toBe(false);
+    expect(switchTimer.timers[TIMER_ID.ONE].msLeft).toBe(1000);
+    expect(switchTimer.timers[TIMER_ID.TWO].isActive).toBe(false);
+    expect(switchTimer.timers[TIMER_ID.TWO].msLeft).toBe(1000);
+    expect(alertSpy).toBeCalled();
   });
 });
